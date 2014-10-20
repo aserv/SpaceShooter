f3009f1c44a202d1a764b05f833693fd3bfb488b
aserv <alexs9@vt.edu>
2014-10-20 09:04:30 -0400

    Added Health Bar to MainGUI
    Health Bar added. ToDo: Add another inner bar (blue maybe?) for use by
    the EXP bar instead of red.

--- Assets/Scripts/MainGUI.cs
+++ Assets/Scripts/MainGUI.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class MainGUI : MonoBehaviour {
-	public Texture expBarTexture;
+	public Texture barTexture;
 	public Rect normalizedExpBar;
+	public Rect normalizedHealthBar;
 	private Rect pixelExpBar;
+	private Rect pixelHealthBar;
 	private PlayerShipController player;
 	private Manager manager;
 	private float expPercent;
@@ -13,15 +15,23 @@ public class MainGUI : MonoBehaviour {
 	void Start () {
 		pixelExpBar = new Rect (normalizedExpBar.x * camera.pixelWidth, normalizedExpBar.y * camera.pixelHeight, 
 			normalizedExpBar.width * camera.pixelWidth, normalizedExpBar.height * camera.pixelHeight);
+		pixelHealthBar = new Rect (normalizedHealthBar.x * camera.pixelWidth, normalizedHealthBar.y * camera.pixelHeight,
+		    normalizedHealthBar.width * camera.pixelWidth, normalizedHealthBar.height * camera.pixelHeight);
 		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerShipController> ();
 		manager = GameObject.FindGameObjectWithTag ("GameController").GetComponent<Manager> ();
 	}
 	
 	void OnGUI () {
 		expPercent = manager.expPercent;
-		GUI.DrawTextureWithTexCoords (pixelExpBar , expBarTexture, new Rect (0, 0.75F, 1, 0.25F));
-		GUI.DrawTextureWithTexCoords (RectSqueeseLeft(pixelExpBar, expPercent), expBarTexture, new Rect (0, 0.5F, expPercent, 0.25F));
-		GUI.DrawTextureWithTexCoords (RectSqueeseRight(pixelExpBar, expPercent), expBarTexture, new Rect (expPercent, 0.25F, 1 - expPercent, 0.25F));
+		healthPercent = player.healthPercent;
+
+		GUI.DrawTextureWithTexCoords (pixelExpBar , barTexture, new Rect (0, 0.75F, 1, 0.25F));
+		GUI.DrawTextureWithTexCoords (RectSqueeseLeft(pixelExpBar, expPercent), barTexture, new Rect (0, 0.5F, expPercent, 0.25F));
+		GUI.DrawTextureWithTexCoords (RectSqueeseRight(pixelExpBar, expPercent), barTexture, new Rect (expPercent, 0.25F, 1 - expPercent, 0.25F));
+
+		GUI.DrawTextureWithTexCoords (pixelHealthBar, barTexture, new Rect (0, 0.75F, 1, 0.25F));
+		GUI.DrawTextureWithTexCoords (RectSqueeseLeft (pixelHealthBar, healthPercent), barTexture, new Rect (0, 0.5F, healthPercent, 0.25F));
+		GUI.DrawTextureWithTexCoords (RectSqueeseRight (pixelHealthBar, healthPercent), barTexture, new Rect (healthPercent, 0.25F, 1 - healthPercent, 0.25F));
 	}
 
 	Rect RectSqueeseLeft(Rect rect, float percent) {
